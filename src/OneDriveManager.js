c3136eddb8a1b5da6654abc159b7c80e7b746032
Apex <32912837+ApexOrder@users.noreply.github.com>
2025-06-12 14:47:11 +0100

    Create OneDriveManager.js

---
+++ src/OneDriveManager.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { loginRequest } from "./authConfig";
+import { PublicClientApplication } from "@azure/msal-browser";
+import axios from "axios";
+
+const msalInstance = new PublicClientApplication({
+  auth: {
+    clientId: authConfig.clientId,
+    authority: authConfig.authority,
+    redirectUri: authConfig.redirectUri
+  }
+});
+
+const OneDriveManager = () => {
+  const [token, setToken] = useState(null);
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    msalInstance.loginPopup({ scopes: authConfig.scopes }).then(resp => {
+      setToken(resp.accessToken);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!token) return;
+    axios.get("https://graph.microsoft.com/v1.0/me/drive/root/children", {
+      headers: { Authorization: `Bearer ${token}` }
+    }).then(res => {
+      setFiles(res.data.value);
+    });
+  }, [token]);
+
+  const convertToPdf = async (itemId, name) => {
+    const res = await axios.get(`https://graph.microsoft.com/v1.0/me/drive/items/${itemId}/content?format=pdf`, {
+      responseType: "blob",
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    const url = URL.createObjectURL(res.data);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = name.replace(/\.docx$/, ".pdf");
+    a.click();
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>My OneDrive Files</h2>
+      <ul>
+        {files.map(file => (
+          <li key={file.id}>
+            {file.name} 
+            {file.name.endsWith(".docx") && (
+              <button onClick={() => convertToPdf(file.id, file.name)}>Convert to PDF</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default OneDriveManager;
